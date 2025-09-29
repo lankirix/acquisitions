f1a633c7c9de3be6e6d797a279f4868ddf2c6cd5
lankirix <lankirix@gmail.com>
2025-09-29 14:26:06 +0100

    implemented ESLint and Preetier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("Hello from LANKIRIX")
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from LANKIRIX');
 });
 
 export default app;
